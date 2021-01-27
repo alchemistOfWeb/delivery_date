2fb4ee49ce53bed38842a4b9f102046f79ac9480
root <root@localhost.localdomain>
2021-01-27 15:29:51 +0400

    first commit

---
+++ app/addons/delivery_date/func.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+// function fn_delivery_date_pre_get_cart_product_data(
+//     $hash, 
+//     $product, 
+//     $skip_promotion, 
+//     $cart, 
+//     $auth, 
+//     $promotion_amount, 
+//     &$fields, 
+//     $join, 
+//     $params
+// ) 
+// {
+    
+//     array_push($fields, '?:products.delivery_timestamp');
+    
+// }
